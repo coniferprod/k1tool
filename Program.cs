a7f67392454e264aba56768efb33d037da4feea3
Jere Käpyaho <jere@coniferproductions.com>
2019-09-01 18:02:36 +0300

    Added .NET Core console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k1tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
